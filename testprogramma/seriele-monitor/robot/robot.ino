8c93eec0f3aee6d8ae775b191d1e86bdcf23751e
Joppe Hoekstra <joppehoekstra@icloud.com>
2016-01-07 10:28:37 +0100

    Update robot.ino

--- testprogramma/seriele-monitor/robot/robot.ino
+++ testprogramma/seriele-monitor/robot/robot.ino
@@ -2,16 +2,28 @@
 #include <JeeLib.h>
 #include <connect.h>
 
-const byte apparaat = 2; // robot = 2, remote = 1, GEEN ID!
+// Var/const voor zenden en ontvangen
+MilliTimer sendTimer; // Om getimed te zenden.
+const byte network = 100;
+const byte Receiver = 2;
+const byte freq = RF12_868MHZ;
+const byte RF12_NORMAL_SENDWAIT = 0;
+
+connect_structBericht bericht;
 
 void setup() {
 
   Serial.begin(9600);
   Serial.println("robot");
-
-  randomSeed(analogRead(0));
+  
 }
 
 void loop(){
-
+  
+  // ontvang bericht
+  if (rf12_recvDone() && rf12_crc == 0 && (rf12_hdr & RF12_HDR_CTL) == 0) {
+    bericht = *(connect_structBericht*)rf12_data;
+    Serial.println(bericht.inhoud);
+  }
+  
 }
