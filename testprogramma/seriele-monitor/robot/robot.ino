eaf0421c73ab3a8d387e8da8ff371eaa5cf8c4fb
Joppe Hoekstra <joppehoekstra@icloud.com>
2016-01-07 10:22:42 +0100

    Update robot.ino

--- testprogramma/seriele-monitor/robot/robot.ino
+++ testprogramma/seriele-monitor/robot/robot.ino
@@ -1,1 +1,17 @@
+// Libraries
+#include <JeeLib.h>
+#include <connect.h>
 
+const byte apparaat = 2; // robot = 2, remote = 1, GEEN ID!
+
+void setup() {
+
+  Serial.begin(9600);
+  Serial.println("robot");
+
+  randomSeed(analogRead(0));
+}
+
+void loop(){
+
+}
