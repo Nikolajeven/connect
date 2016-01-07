63cfc3577d4f566cb6ee8503c47c5b0fb2220cf4
Joppe Hoekstra <joppehoekstra@icloud.com>
2016-01-07 10:20:06 +0100

    Update remote.ino

--- testprogramma/seriele-monitor/remote/remote.ino
+++ testprogramma/seriele-monitor/remote/remote.ino
@@ -1,1 +1,34 @@
+#include <JeeLib.h>
+#include <connect.h>
 
+#include "struct.h"
+#include "joystick.h"
+
+const byte apparaatID = 1;
+
+structBericht bericht;
+
+void setup() {
+
+   // Om de A/D conversie veel sneller te laten lopen. Scheelt 0.33 msec per conversie
+   bitClear(ADCSRA,ADPS0);
+   bitClear(ADCSRA,ADPS1);
+   bitSet(ADCSRA,ADPS2);
+
+   Serial.begin(9600);
+   rf12_initialize(apparaatID, freq, network, 1600);
+
+   Serial.println("...");
+   Serial.println("connect - remote");
+}
+
+
+void loop() {
+
+   bericht.afzender = apparaatID;
+
+   bericht.inhoud = 123;
+
+   zend(bericht);
+
+}
